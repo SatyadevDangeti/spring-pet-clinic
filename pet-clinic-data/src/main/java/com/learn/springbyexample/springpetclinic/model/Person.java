6d92262eccffe4f658d4219bd1ea7364fdac0e4e
satyadevdangeti <satyadevdangeti22@gmail.com>
2020-09-01 17:43:55 -0500

    Added Base Entity.

--- pet-clinic-data/src/main/java/com/learn/springbyexample/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/learn/springbyexample/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.learn.springbyexample.springpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/com/learn/springbyexample/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/learn/springbyexample/springpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.learn.springbyexample.springpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
     private Owner owner;
     private PetType petType;
     private LocalDate birthDate;
